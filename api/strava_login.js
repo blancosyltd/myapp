5a51af5aec9e2ad5104aa7f41421d5544c4e5362
koinangestreets11 <koinangestreets11@gmail.com>
2025-08-13 19:13:25 +0300

    e

--- api/strava_login.js
+++ api/strava_login.js
@@ -1,7 +1,7 @@
 // /api/auth/strava_login.js
 export default function handler(req, res) {
   const clientId = process.env.STRAVA_CLIENT_ID;
-  const redirectUri = `${process.env.BASE_URL}/api/auth/strava_callback`;
+  const redirectUri = `${process.env.BASE_URL}/api/strava_callback`;
   const scope = "read,activity:read_all"; // Scope for workouts
 
   const authUrl = `https://www.strava.com/oauth/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}`;
